794b7b8fcf1ff9758de6d5b56bb799c34f93d147
hanranti <hanranti@cs.helsinki.fi>
2021-01-21 12:36:16 +0200

    api-url for production

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:5000/api'
+// Tuotantoympäristössä api:n polku alkaa medified/...
+const productionPath = process.env.NODE_ENV === 'production' ? 'medified/' : ''
+const baseUrl = `http://localhost:5000/${productionPath}api`
 
-const getAll = () => axios.get(`${baseUrl}/patients`).then(response => response.data)
+const getAll = () => axios.get(`${baseUrl}/patients`).then(response => {
+    console.log(process.env.NODE_ENV)
+    return response.data
+}).catch(error => console.log(process.env.NODE_ENV + error + baseUrl))
 
 export default { getAll }
