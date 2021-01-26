ee0aa7b4dce07a67b30f0a203f63740012747f42
hanranti <hanranti@cs.helsinki.fi>
2021-01-26 18:01:03 +0200

    apitest fixed configtest added

---
+++ backend/tests/config.test.js
@@ -0,0 +1,8 @@
+const { TestScheduler } = require('jest')
+const config = require('../utils/config')
+
+test('Backend PORT of 5000', () => {
+    const result = config.PORT
+
+    expect(result).toBe(5000)
+})
