b8a399f9c2b10952ca1965598e4dc942fd0803d2
hanranti <hanranti@cs.helsinki.fi>
2021-01-26 17:47:26 +0200

    app as separate file

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const middleware = require('./utils/middleware')
+const patientsRouter = require('./controllers/patientsRouter')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+// Tama tarvitaan, jos frontend buildataan backendiin
+//app.use(express.static('build'))
+
+// JSON luettavaan muotoon
+app.use(express.json())
+
+// Morgan logit HTTP-pyynnöistä
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan('tiny'))
+app.use(morgan(':body'))
+
+app.use(cors())
+
+// Tietokannan url
+const patientsUrl = '/api/patients'
+// /api/patients/ polkujen käsittely
+app.use(patientsUrl, patientsRouter)
+// Pyyntoihin ylla maarittelemattomista poluista palautetaan 404
+app.use(middleware.unknownEndpoint)
+// Virheidenkasittely
+app.use(middleware.errorHandler)
+
+module.exports = app
--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,8 @@
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const patientsRouter = require('./controllers/patientsRouter')
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
 
-const app = express()
+const server = http.createServer(app)
 
-// Tama tarvitaan, jos frontend buildataan backendiin
-//app.use(express.static('build'))
-
-// JSON luettavaan muotoon
-app.use(express.json())
-
-// Morgan logit HTTP-pyynnöistä
-morgan.token('body', (req) => JSON.stringify(req.body))
-app.use(morgan('tiny'))
-app.use(morgan(':body'))
-
-app.use(cors())
-
-// Tietokannan url
-const patientsUrl = '/api/patients'
-// /api/patients/ polkujen käsittely
-app.use(patientsUrl, patientsRouter)
-// Pyyntoihin ylla maarittelemattomista poluista palautetaan 404
-app.use(middleware.unknownEndpoint)
-// Virheidenkasittely
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => logger.log(`Backend is running in port ${config.PORT}`))
-
-module.exports = app
+server.listen(config.PORT, () => logger.log(`Backend is running in port ${config.PORT}`))
