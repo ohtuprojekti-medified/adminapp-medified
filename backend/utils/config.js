17307805f7cf0989166810ea29985fd49e81eb84
hanranti <hanranti@cs.helsinki.fi>
2021-01-27 09:40:48 +0200

    DB_URI:s as comments

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,6 +2,12 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 
+// Tietokannan URI:t
+/*const DB_URI = undefined
+const TEST_DB_URI = undefined*/
+
 module.exports = {
-  PORT
+  PORT,
+  //  DB_URI,
+  //  TEST_DB_URI
 }
