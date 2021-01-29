e1175fe70346ecdaf67f29e1485686f3026452ed
noorarytila <rytilanoora@gmail.com>
2021-01-29 10:03:25 +0200

    added docker container for postgres

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,6 +2,7 @@ const config = require('../utils/config')
 
 // Sequelize käyttöön
 const Sequelize = require('sequelize')
+
 const sequelize = new Sequelize(config.db_name, config.db_user, config.db_password, {
   host: config.db_host,
   dialect: config.dialect,
