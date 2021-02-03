44ef8eb30919ffd55ade091eacc62760586abd0c
hanranti <hanranti@cs.helsinki.fi>
2021-02-03 14:31:05 +0200

    Add cypress test with working eslint

--- frontend/cypress/integration/login.spec.js
+++ frontend/cypress/integration/login.spec.js
@@ -1,8 +1,6 @@
-import { scryRenderedComponentsWithType } from "react-dom/test-utils"
-
-describe('Login', () => {
-    it('dissappears after logged in', () => {
-        cy.visit('http://localhost:3000/')
-        cy.contains('Adminapp for monitoring moods')
-    })
+describe('Login', function () {
+  it('dissappears after logged in', function () {
+    cy.visit('http://localhost:3000/')
+    cy.contains('Adminapp for monitoring moods')
+  })
 })
