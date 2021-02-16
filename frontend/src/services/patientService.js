65573c74fdf134339d90d01d9f3f169e729e9028
Vikke Tiirola <vikke.tiirola@gmail.com>
2021-02-16 11:10:03 +0200

    Update backend url for frontend production build

--- frontend/src/services/patientService.js
+++ frontend/src/services/patientService.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 // Needed for production environment
-const productionPath = process.env.NODE_ENV === 'production' ? 'medified/' : ''
-const baseUrl = `http://localhost:5000/${productionPath}api`
+const baseUrl = process.env.NODE_ENV === 'production' 
+    ? 'http://localhost:3000/medified/api' 
+    : 'http://localhost:5000/api'
 
 const getAll = () => axios.get(`${baseUrl}/patients`).then(response => response.data)
 
