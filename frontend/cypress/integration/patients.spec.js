3836fee7f0eb4d0e7b50b921640e6a64f0a87a68
hanranti <hanranti@cs.helsinki.fi>
2021-02-18 13:43:22 +0200

    Add temporary login command for cypress tests
    This adds a login-function for patients.spec.js that can be used to login
    automatically. This can be modified to use the cy.login-command when it
    works for faster logging in during tests.

--- frontend/cypress/integration/patients.spec.js
+++ frontend/cypress/integration/patients.spec.js
@@ -1,17 +1,23 @@
 const testUsername = Cypress.env('USERNAME')
 const testPassword = Cypress.env('PASSWORD')
 
+// Helper functions
+const login = (username, password) => {
+  cy.visit('http://localhost:3000/')
+  cy.get('#username').type(username)
+  cy.get('#password').type(password)
+  cy.contains('login').click()
+  // Replace above with this when it works
+  //cy.login({ username, password })
+}
+
 describe('Patients', function () {
   beforeEach(function () {
-    //cy.login({ testUsername, testPassword })
-    //cy.log(localStorage.getItem('loggedUser'))
-    cy.visit('http://localhost:3000/')
+    login(testUsername, testPassword)
   })
 
   it('exists', function () {
-    cy.log(testUsername)
-    cy.log(testPassword)
     cy.contains('Adminapp for monitoring moods')
-    //cy.contains('Patients moods listed')
+    cy.contains('Patients moods listed')
   })
 })
