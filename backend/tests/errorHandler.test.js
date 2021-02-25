7efe07fbee9c58830df6f08efb0fe82030ced6f5
hanranti <hanranti@cs.helsinki.fi>
2021-02-25 13:51:04 +0200

    Create better tests for errorhandling

--- backend/tests/errorHandler.test.js
+++ backend/tests/errorHandler.test.js
@@ -1,27 +1,47 @@
-const mongoose = require('mongoose')
+const mocks = require('node-mocks-http')
 const sinon = require('sinon')
-const supertest = require('supertest')
-let app
-let api
-let middlewares
 
-let nextMock
+let errorHandler
+let error, mockReq, mockRes, mockNext
+let mockSend, mockJson
 
 beforeEach(() => {
-  nextMock = jest.fn()
-  middlewares = require('../utils/middleware')
-  sinon.stub(middlewares, 'errorHandler')
-    .callsFake((error, req, res) => {
-      middlewares.errorHandler(error, req, res, nextMock)
-    })
-  app = require('../app')
-  api = supertest(app)
+  errorHandler = require('../utils/middleware').errorHandler
+  mockReq = mocks.createRequest()
+  mockSend = sinon.spy()
+  mockJson = sinon.spy()
+  mockRes = mocks.createRequest({
+    status: () => {
+      return {
+        send: mockSend,
+        json: mockJson
+      }
+    }
+  })
+  mockNext = sinon.spy()
 })
 
-// This does not work yet
 test('errorHandler calls next when no error', async () => {
-  await api.get('/api/patients')
-  //expect(nextMock).toHaveBeenCalledTimes(1)
+  error = {}
+  errorHandler(error, mockReq, mockRes, mockNext)
+  expect(mockNext.called).toBe(true)
 })
 
-afterAll(() => mongoose.connection.close())
+test('errorHandler sends error when CastError is made', async () => {
+  error = {
+    name: 'CastError'
+  }
+  errorHandler(error, mockReq, mockRes, mockNext)
+  expect(mockSend.called).toBe(true)
+  expect(mockSend.calledWith({ error: 'Id in URL is not correct' }))
+})
+
+test('errorHandler sends error when CastError is made', async () => {
+  error = {
+    name: 'ValidationError',
+    message: 'Error with validation!'
+  }
+  errorHandler(error, mockReq, mockRes, mockNext)
+  expect(mockJson.called).toBe(true)
+  expect(mockJson.calledWith({ error: 'Error with validation!' }))
+})
