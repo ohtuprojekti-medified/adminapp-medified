cf0b41ced8a0be7bbea157bf9e3950a254326e92
hanranti <hanranti@cs.helsinki.fi>
2021-03-03 10:16:07 +0200

    Add jsdoc for config test

--- backend/tests/config.test.js
+++ backend/tests/config.test.js
@@ -1,5 +1,21 @@
+/**
+ * Tests for config
+ *
+ * @module config_test
+ * @requires utils/config
+ */
 const config = require('../utils/config')
 
+/**
+ * Tests that the port of backend is correct
+ *
+ * @name test_backend_port
+ * @function
+ * @memberof module:config_test
+ * @inner
+ * @param {string} name - Name of the test
+ * @param {object} test - Test code
+ */
 test('Backend PORT of 5000', () => {
   const result = config.PORT
 
