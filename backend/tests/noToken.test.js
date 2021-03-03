ffcb4527d79540e14eae3b7cb434c413d7b4f074
hanranti <hanranti@cs.helsinki.fi>
2021-03-03 10:45:03 +0200

    Add jsdoc to noToken and unknownEndpoint tests

--- backend/tests/noToken.test.js
+++ backend/tests/noToken.test.js
@@ -1,11 +1,70 @@
+/**
+ * Tests for requests to backend when no token is present
+ *
+ * @module noToken_test
+ * @requires mongoose
+ * @requires supertest
+ * @requires app
+ */
+
+/**
+ * Used to make requests to the backend
+ *
+ * @type {object}
+ * @constant
+ * @namespace mongoose
+ */
 const mongoose = require('mongoose')
+
+/**
+ * Used to create backend for tests
+ *
+ * @type {object}
+ * @constant
+ * @namespace supertest
+ */
 const supertest = require('supertest')
+
+/**
+ * The backend of the app
+ *
+ * @type {object}
+ * @constant
+ * @namespace app
+ */
 const app = require('../app')
+
+/**
+ * Api for tests created with supertest
+ *
+ * @type {object}
+ * @constant
+ * @namespace api
+ */
 const api = supertest(app)
 
-test('patients are not returned without token', async () => {
+/**
+ * Tests that users are not returned without token
+ *
+ * @name no_users_without_token
+ * @function
+ * @memberof module:noToken_test
+ * @inner
+ * @param {string} name - Name of the test
+ * @param {object} test - Test code
+ */
+test('users are not returned without token', async () => {
   await api.get('/api/users')
     .expect(403)
 })
 
+/**
+ * Close connection after all tests have been executed
+ *
+ * @name afterAll
+ * @function
+ * @memberof module:noToken_test
+ * @inner
+ * @param {object} functionAfterAll - Function to be executed after all tests
+ */
 afterAll(() => mongoose.connection.close())
--- backend/tests/unknownEndpoint.test.js
+++ backend/tests/unknownEndpoint.test.js
@@ -1,9 +1,42 @@
+/**
+ * Tests for unknownEndpoint middleware
+ *
+ * @module unknownEndpoint_test
+ * @requires node-mocks-http
+ * @requires sinon
+ * @requires utils/middleware
+ */
+
+/**
+ * Mocks for request and response in express
+ *
+ * @type {object}
+ * @constant
+ * @namespace mocks
+ */
 const mocks = require('node-mocks-http')
+
+/**
+ * Mock for next-function in express
+ *
+ * @type {object}
+ * @constant
+ * @namespace sinon
+ */
 const sinon = require('sinon')
 let unknownEndpoint
 let mockReq, mockRes, mockNext
 let mockSend
 
+/**
+ * Restore unknownEndpoint and mocks before each test
+ *
+ * @name beforeEach
+ * @function
+ * @memberof module:unknownEndpoint_test
+ * @inner
+ * @param {object} functionBeforeEach - Function to be executed before each test
+ */
 beforeEach(() => {
   unknownEndpoint = require('../utils/middleware').unknownEndpoint
   mockReq = mocks.createRequest()
@@ -17,6 +50,16 @@ beforeEach(() => {
   })
 })
 
+/**
+ * Tests that middleware responds with error
+ *
+ * @name unknownEndpoint
+ * @function
+ * @memberof module:unknownEndpoint_test
+ * @inner
+ * @param {string} name - Name of the test
+ * @param {object} test - Test code
+ */
 test('unknownEndpoint sends error', async () => {
   unknownEndpoint(mockReq, mockRes, mockNext)
   expect(mockSend.calledWith({ error: 'URL path does not match anything' })).toBe(true)
