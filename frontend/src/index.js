da21753cc131fc66f9b66f27ac66c12d97e8ffb1
Vikke Tiirola <vikke.tiirola@gmail.com>
2021-03-14 18:47:02 +0200

    Add default api url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import axios from 'axios'
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/'
 
 /**
  * Run the app
