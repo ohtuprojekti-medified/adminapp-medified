f570f080e9403a2c68a004eb248eb54b3d4f3cf6
hanranti <hanranti@cs.helsinki.fi>
2021-03-25 11:13:22 +0200

    Add test for average retention rate
    This adds more data for the tests and a new test for average retention
    rate.

--- backend/tests/retentionrateController.test.js
+++ backend/tests/retentionrateController.test.js
@@ -5,9 +5,13 @@ let user_activities_stub, user_profiles_stub
 
 const retentionrateControllerMocked = () => {
   db = require('../models')
-  const TIME3 = new Date(new Date() - 604800000)
-  const TIME2 = new Date(new Date() - 1512000000)
-  const TIME1 = new Date(new Date() - 2177280000)
+  const TIME6 = new Date(new Date() - 604800000)
+  const TIME5 = new Date(new Date() - 1512000010)
+  const TIME4 = new Date(new Date() - 2177280020)
+  const TIME3 = new Date(new Date() - 2782080030)
+  const TIME2 = new Date(new Date() - 3386880040)
+  const TIME1 = new Date(new Date() - 3991680050)
+
   user_activities_stub = sinon.stub(db.user_activities, 'findAll')
     .callsFake(() => {
       return [
@@ -31,6 +35,48 @@ const retentionrateControllerMocked = () => {
             user_id: 'user21',
             created_at: TIME3
           }
+        },
+        {
+          dataValues: {
+            id: 24,
+            user_id: 'user21',
+            created_at: TIME4
+          }
+        },
+        {
+          dataValues: {
+            id: 25,
+            user_id: 'user21',
+            created_at: TIME5
+          }
+        },
+        {
+          dataValues: {
+            id: 26,
+            user_id: 'user21',
+            created_at: TIME6
+          }
+        },
+        {
+          dataValues: {
+            id: 27,
+            user_id: 'user22',
+            created_at: TIME4
+          }
+        },
+        {
+          dataValues: {
+            id: 28,
+            user_id: 'user22',
+            created_at: TIME5
+          }
+        },
+        {
+          dataValues: {
+            id: 29,
+            user_id: 'user22',
+            created_at: TIME6
+          }
         }
       ]
     })
@@ -47,6 +93,18 @@ const retentionrateControllerMocked = () => {
           last_name: 'Ittam',
           added_organisation: 'OHTU'
         }
+      },
+      {
+        dataValues: {
+          user_id: 'user22',
+          height: '',
+          weight: '',
+          sex: null,
+          birth_date: null,
+          first_name: 'Maija',
+          last_name: 'Ajiam',
+          added_organisation: 'OHTU'
+        }
       }]
     })
   retentionrateController = require('../controllers/retentionrateController')
@@ -58,8 +116,12 @@ describe('retentionrate controller', () => {
     retentionrateController = retentionrateControllerMocked()
   })
 
-  test('returns correct data', async () => {
-    expect(await retentionrateController.findRetentionRates()).toEqual([{ daysUsed: 7 }])
+  test('findRetentionRates returns correct data', async () => {
+    expect(await retentionrateController.findRetentionRates()).toEqual([{ daysUsed: 28 }, { daysUsed: 7 }])
+  })
+
+  test('findAverageRetentionRate returns correct average', async () => {
+    expect(await retentionrateController.findAverageRetentionRate()).toEqual(17.5)
   })
 
   afterEach(() => {
