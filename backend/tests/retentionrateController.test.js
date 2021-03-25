31308d5c9232beb248252161b00354e115b07d69
hanranti <hanranti@cs.helsinki.fi>
2021-03-25 10:29:29 +0200

    Complete test for retentionRateController
    This adds a test that uses sinon stub as database

--- backend/tests/retentionrateController.test.js
+++ backend/tests/retentionrateController.test.js
@@ -5,37 +5,49 @@ let user_activities_stub, user_profiles_stub
 
 const retentionrateControllerMocked = () => {
   db = require('../models')
-  user_activities_stub = sinon.stub(db, 'user_activities')
+  const TIME3 = new Date(new Date() - 604800000)
+  const TIME2 = new Date(new Date() - 1512000000)
+  const TIME1 = new Date(new Date() - 2177280000)
+  user_activities_stub = sinon.stub(db.user_activities, 'findAll')
     .callsFake(() => {
-      return {
-        findAll: () => {
-          return {
-            dataValues: {
-              id: 21,
-              user_id: 'user21'
-            }
+      return [
+        {
+          dataValues: {
+            id: 21,
+            user_id: 'user21',
+            created_at: TIME1
+          }
+        },
+        {
+          dataValues: {
+            id: 22,
+            user_id: 'user21',
+            created_at: TIME2
+          }
+        },
+        {
+          dataValues: {
+            id: 23,
+            user_id: 'user21',
+            created_at: TIME3
           }
         }
-      }
+      ]
     })
-  user_profiles_stub = sinon.stub(db, 'user_profiles')
+  user_profiles_stub = sinon.stub(db.user_profiles, 'findAll')
     .callsFake(() => {
-      return {
-        findAll: () => {
-          return {
-            dataValues: {
-              user_id: 'user21',
-              height: '',
-              weight: '',
-              sex: null,
-              birth_date: null,
-              first_name: 'Matti',
-              last_name: 'Ittam',
-              added_organisation: 'OHTU'
-            }
-          }
+      return [{
+        dataValues: {
+          user_id: 'user21',
+          height: '',
+          weight: '',
+          sex: null,
+          birth_date: null,
+          first_name: 'Matti',
+          last_name: 'Ittam',
+          added_organisation: 'OHTU'
         }
-      }
+      }]
     })
   retentionrateController = require('../controllers/retentionrateController')
   return retentionrateController
@@ -47,7 +59,7 @@ describe('retentionrate controller', () => {
   })
 
   test('returns correct data', async () => {
-    expect(retentionrateController.findRetentionRates()).toEqual([{ daysUsed: 1 }])
+    expect(await retentionrateController.findRetentionRates()).toEqual([{ daysUsed: 7 }])
   })
 
   afterEach(() => {
