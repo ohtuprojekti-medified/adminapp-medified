5e3a3bcb90b22c1ee2e16093ff6523ef44d04e30
noorarytila <rytilanoora@gmail.com>
2021-03-26 20:48:31 +0200

    Added backend tests for cumulative new users and active users

--- backend/tests/userhistoryController.test.js
+++ backend/tests/userhistoryController.test.js
@@ -1,15 +1,18 @@
 const sinon = require('sinon')
 let userhistoryController, db, user_activities_stub, user_profiles_stub
+const newDates = require('./newDatesAroundLastMidnight')
 
 const userhistoryControllerMocked = () => {
   db = require('../models')
 
-  const TIME6 = new Date(new Date() - 604800000)
-  const TIME5 = new Date(new Date() - 1512000010)
-  const TIME4 = new Date(new Date() - 2177280020)
-  const TIME3 = new Date(new Date() - 2782080030)
-  const TIME2 = new Date(new Date() - 3386880040)
-  const TIME1 = new Date(new Date() - 3991680050)
+  // const TIME6 = new Date(new Date() - 604800000)
+  // const TIME5 = new Date(new Date() - 1512000010)
+  // const TIME4 = new Date(new Date() - 2177280020)
+  // const TIME3 = new Date(new Date() - 2782080030)
+  // const TIME2 = new Date(new Date() - 3386880040)
+  // const TIME1 = new Date(new Date() - 3991680050)
+  const TIMES1 = newDates([-46.2, -39.2, -32.2, -25.2, -17.5, -7])
+  const TIMES2 = newDates([-25.4, -17.6, -7])
 
   user_activities_stub = sinon.stub(db.user_activities, 'findAll')
     .callsFake(() => {
@@ -18,63 +21,63 @@ const userhistoryControllerMocked = () => {
           dataValues: {
             id: 21,
             user_id: 'user21',
-            created_at: TIME1
+            created_at: TIMES1[0]
           }
         },
         {
           dataValues: {
             id: 22,
             user_id: 'user21',
-            created_at: TIME2
+            created_at: TIMES1[1]
           }
         },
         {
           dataValues: {
             id: 23,
             user_id: 'user21',
-            created_at: TIME3
+            created_at: TIMES1[2]
           }
         },
         {
           dataValues: {
             id: 24,
             user_id: 'user21',
-            created_at: TIME4
+            created_at: TIMES1[3]
           }
         },
         {
           dataValues: {
             id: 25,
             user_id: 'user21',
-            created_at: TIME5
+            created_at: TIMES1[4]
           }
         },
         {
           dataValues: {
             id: 26,
             user_id: 'user21',
-            created_at: TIME6
+            created_at: TIMES1[5]
           }
         },
         {
           dataValues: {
             id: 27,
             user_id: 'user22',
-            created_at: TIME4
+            created_at: TIMES1[6]
           }
         },
         {
           dataValues: {
             id: 28,
             user_id: 'user22',
-            created_at: TIME5
+            created_at: TIMES1[0]
           }
         },
         {
           dataValues: {
             id: 29,
             user_id: 'user22',
-            created_at: TIME6
+            created_at: TIMES1[2]
           }
         }
       ]
@@ -91,7 +94,7 @@ const userhistoryControllerMocked = () => {
           first_name: 'Matti',
           last_name: 'Ittam',
           added_organisation: 'OHTU',
-          created_at: TIME1
+          created_at: TIMES2[0]
         }
       },
       {
@@ -104,7 +107,7 @@ const userhistoryControllerMocked = () => {
           first_name: 'Maija',
           last_name: 'Ajiam',
           added_organisation: 'OHTU',
-          created_at: TIME2
+          created_at: TIMES2[1]
         }
       },
       {
@@ -117,7 +120,7 @@ const userhistoryControllerMocked = () => {
           first_name: 'Mikko',
           last_name: 'Mallikas',
           added_organisation: 'OHTU',
-          created_at: TIME6
+          created_at: TIMES2[2]
         }
       }]
     })
@@ -137,7 +140,7 @@ describe('userhistory controller', () => {
     expect(cumulativeUserActivities[1].entries).toEqual(3)
   })
 
-  test('findUserActivities return correct data', async () => {
+  test('findUserActivities returns correct data', async () => {
     const activeUsers = await userhistoryController.findActiveUsers()
     expect(activeUsers[0].entries).toEqual(1)
     expect(activeUsers[activeUsers.length-1].entries).toEqual(2)
