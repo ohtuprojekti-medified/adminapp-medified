d9f5e4dd0d2717b178d39bd5b3e950cd70e838b2
hanranti <hanranti@cs.helsinki.fi>
2021-04-15 15:59:47 +0300

    Add e2e tests

--- cypress/integration/caregivers.spec.js
+++ cypress/integration/caregivers.spec.js
@@ -76,4 +76,33 @@ describe('Caregivers', function () {
     cy.contains('Caregivers')
     cy.contains('Registered caregivers: 5')
   })
+
+  it('exists after selecting start date in filters', function () {
+    cy.contains('Filter').click()
+    cy.get('[data-testid="startDate-checkbox"]').check()
+    cy.get('[data-testid="startDate-date"]').type('2020-06-01')
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Caregivers')
+    cy.contains('Registered caregivers: 5')
+  })
+
+  it('exists after selecting end date in filters', function () {
+    cy.contains('Filter').click()
+    cy.get('[data-testid="endDate-checkbox"]').check()
+    cy.get('[data-testid="endDate-date"]').type('2020-11-01')
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Caregivers')
+    cy.contains('Registered caregivers: 5')
+  })
+
+  it('exists after selecting start date and end date in filters', function () {
+    cy.contains('Filter').click()
+    cy.get('[data-testid="startDate-checkbox"]').check()
+    cy.get('[data-testid="startDate-date"]').type('2020-06-01')
+    cy.get('[data-testid="endDate-checkbox"]').check()
+    cy.get('[data-testid="endDate-date"]').type('2020-11-01')
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Caregivers')
+    cy.contains('Registered caregivers: 5')
+  })
 })
