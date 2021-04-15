cdb4e1ca2a09a5c988c16b2ba05f51f30d539fea
hanranti <hanranti@cs.helsinki.fi>
2021-04-15 17:42:12 +0300

    Fix lint

--- frontend/src/components/TimeFilter.test.js
+++ frontend/src/components/TimeFilter.test.js
@@ -4,57 +4,57 @@ import { render, waitFor, fireEvent } from '@testing-library/react'
 import TimeFilter from './TimeFilter'
 
 describe('<TimeFilter />', () => {
-    let component
-    const handleStartDateEnableChange = jest.fn()
-    const handleEndDateEnableChange = jest.fn()
-    const handleStartDateChange = jest.fn()
-    const handleEndDateChange = jest.fn()
+  let component
+  const handleStartDateEnableChange = jest.fn()
+  const handleEndDateEnableChange = jest.fn()
+  const handleStartDateChange = jest.fn()
+  const handleEndDateChange = jest.fn()
 
-    component = render(<TimeFilter startDateEnable={false} endDateEnable={false}
-        startDate={''} endDate={''} handleStartDateEnableChange={handleStartDateEnableChange}
-        handleEndDateEnableChange={handleEndDateEnableChange} handleStartDateChange={handleStartDateChange}
-        handleEndDateChange={handleEndDateChange} />)
+  component = render(<TimeFilter startDateEnable={false} endDateEnable={false}
+    startDate={''} endDate={''} handleStartDateEnableChange={handleStartDateEnableChange}
+    handleEndDateEnableChange={handleEndDateEnableChange} handleStartDateChange={handleStartDateChange}
+    handleEndDateChange={handleEndDateChange} />)
 
-    test('renders TimeFilter', () => {
-        waitFor(() => {
-            expect(component.container).toHaveTextContent('Start date')
-            expect(component.container).toHaveTextContent('End date')
-        })
+  test('renders TimeFilter', () => {
+    waitFor(() => {
+      expect(component.container).toHaveTextContent('Start date')
+      expect(component.container).toHaveTextContent('End date')
     })
+  })
 
-    test('checking start date checkbox calls eventhandler', () => {
-        const checkbox = component.findByTestId('startDate-checkbox')
+  test('checking start date checkbox calls eventhandler', () => {
+    const checkbox = component.findByTestId('startDate-checkbox')
 
-        waitFor(() => {
-            fireEvent.click(checkbox)
-            expect(handleStartDateEnableChange.mock.calls).toHaveLength(1)
-        })
+    waitFor(() => {
+      fireEvent.click(checkbox)
+      expect(handleStartDateEnableChange.mock.calls).toHaveLength(1)
     })
+  })
 
-    test('checking end date checkbox calls eventhandler', () => {
-        const checkbox = component.findByTestId('endDate-checkbox')
+  test('checking end date checkbox calls eventhandler', () => {
+    const checkbox = component.findByTestId('endDate-checkbox')
 
-        waitFor(() => {
-            fireEvent.click(checkbox)
-            expect(handleEndDateEnableChange.mock.calls).toHaveLength(1)
-        })
+    waitFor(() => {
+      fireEvent.click(checkbox)
+      expect(handleEndDateEnableChange.mock.calls).toHaveLength(1)
     })
+  })
 
-    test('typeing start date calls eventhandler', () => {
-        const dateInput = component.findByTestId('startDate-date')
+  test('typeing start date calls eventhandler', () => {
+    const dateInput = component.findByTestId('startDate-date')
 
-        waitFor(() => {
-            fireEvent.change(dateInput, { target: { value: '2020-06-01' } })
-            expect(handleStartDateChange.mock.calls).toHaveLength(1)
-        })
+    waitFor(() => {
+      fireEvent.change(dateInput, { target: { value: '2020-06-01' } })
+      expect(handleStartDateChange.mock.calls).toHaveLength(1)
     })
+  })
 
-    test('typeing end date calls eventhandler', () => {
-        const dateInput = component.findByTestId('endDate-date')
+  test('typeing end date calls eventhandler', () => {
+    const dateInput = component.findByTestId('endDate-date')
 
-        waitFor(() => {
-            fireEvent.change(dateInput, { target: { value: '2020-11-01' } })
-            expect(handleStartDateChange.mock.calls).toHaveLength(1)
-        })
+    waitFor(() => {
+      fireEvent.change(dateInput, { target: { value: '2020-11-01' } })
+      expect(handleStartDateChange.mock.calls).toHaveLength(1)
     })
+  })
 })
