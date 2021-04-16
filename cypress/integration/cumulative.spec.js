c57fd411e43cd01393f5fb0b5778c4df16d6b81c
hanranti <hanranti@cs.helsinki.fi>
2021-04-16 21:44:24 +0300

    Add jsdoc for time filter tests

--- cypress/integration/cumulative.spec.js
+++ cypress/integration/cumulative.spec.js
@@ -75,6 +75,16 @@ describe('Cumulative users', function () {
     cy.contains('New users, cumulative and active users weekly')
   })
 
+  /**
+   * Test that cumulative users page exists after selecting start date
+   *
+   * @name Cumulative_exists_after_selecting_start_date
+   * @type {object}
+   * @memberof module:cypress/integration/cumulative_spec
+   * @inner
+   * @param {string} describe - exists after selecting start date
+   * @param {object} testFunction - Function that runs test
+   */
   it('exists after selecting start date in filters', function () {
     cy.contains('Filter').click()
     cy.get('[data-testid="startDate-checkbox"]').check()
@@ -83,6 +93,16 @@ describe('Cumulative users', function () {
     cy.contains('New users, cumulative and active users weekly')
   })
 
+  /**
+   * Test that cumulative users page exists after selecting end date
+   *
+   * @name Cumulative_exists_after_selecting_end_date
+   * @type {object}
+   * @memberof module:cypress/integration/cumulative_spec
+   * @inner
+   * @param {string} describe - exists after selecting end date
+   * @param {object} testFunction - Function that runs test
+   */
   it('exists after selecting end date in filters', function () {
     cy.contains('Filter').click()
     cy.get('[data-testid="endDate-checkbox"]').check()
@@ -91,6 +111,16 @@ describe('Cumulative users', function () {
     cy.contains('New users, cumulative and active users weekly')
   })
 
+  /**
+   * Test that cumulative users page exists after selecting start date and end date
+   *
+   * @name Cumulative_exists_after_selecting_start_date_and_end_date
+   * @type {object}
+   * @memberof module:cypress/integration/cumulative_spec
+   * @inner
+   * @param {string} describe - exists after selecting start date and end date
+   * @param {object} testFunction - Function that runs test
+   */
   it('exists after selecting start date and end date in filters', function () {
     cy.contains('Filter').click()
     cy.get('[data-testid="startDate-checkbox"]').check()
