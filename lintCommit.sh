ecc2e5bd51026985ab49d63a0770a4f18599d5d4
hanranti <hanranti@cs.helsinki.fi>
2021-04-19 13:51:30 +0300

    Add messages to lintcommit script
    Add messages to the script to explain what it is doing and Ask y/n
    question to determine wether to stage and commit changes

--- lintCommit.sh
+++ lintCommit.sh
@@ -1,9 +1,18 @@
 set -e;
-npm run lint-fix
+echo 'Lintting and fixing errors. Exit on error!'
+echo 'Lint-fix root'
+npm run lint-fix;
 cd frontend;
+echo 'Lint-fix frontend'
 npm run lint-fix;
 cd ../backend;
+echo 'Lint-fix backend'
 npm run lint-fix;
 cd ..;
-git add -A;
-git commit
+while true; do
+  read -p 'No errors found! Check for warnings. Commit changes? (y/n)' yn
+  case $yn in
+    [Yy]* ) git add -A; git commit;;
+    [Nn]* ) exit;;
+  esac
+done
