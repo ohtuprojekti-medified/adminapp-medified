033a92872b24c7e0e25a44ea53ce3b893f738967
hanranti <hanranti@cs.helsinki.fi>
2021-04-20 23:29:48 +0300

    Add jsdoc for test

--- frontend/src/components/MoodAverage.test.js
+++ frontend/src/components/MoodAverage.test.js
@@ -1,15 +1,52 @@
+/**
+ * Tests for MoodAverage component
+ *
+ * @module src/components/MoodAverage_test
+ * @requires react
+ * @requires @testing-library/jest-dom/extend-expect
+ * @requires @testing-library/react
+ * @requires src/components/MoodAverage
+ */
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, waitFor } from '@testing-library/react'
 import MoodAverage from './MoodAverage'
 
+/**
+ * Describe tests
+ *
+ * @type {object}
+ * @function
+ * @memberof module:src/components/MoodAverage_test_test
+ * @param {string} description - Description of tests
+ * @param {object} testCode - Code for tests
+ */
 describe('<MoodAverage />', () => {
   let component
 
+  /**
+   * Render MoodAverage
+   *
+   * @type {object}
+   * @function
+   * @memberof module:src/components/MoodAverage_test
+   * @inner
+   * @param {object} beforeEachCode - Code to be run before each test
+   */
   beforeEach(() => {
     component = render(<MoodAverage />)
   })
 
+  /**
+   * Test that MoodAverage is rendered
+   *
+   * @type {object}
+   * @function
+   * @memberof module:src/components/MoodAverage_test
+   * @inner
+   * @param {string} description - renders MoodAverage
+   * @param {object} TestCode - Code that runs the test
+   */
   test('renders MoodAverage', () => {
     waitFor(() => {
       expect(component.container.toHaveTextContent('Adminapp for monitoring moods'))
