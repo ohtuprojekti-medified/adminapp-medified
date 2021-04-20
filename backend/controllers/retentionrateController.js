38aea249d26916d866ec3fda8a98528e70dc75cf
Vikke Tiirola <vikke.tiirola@gmail.com>
2021-04-20 10:38:04 +0300

    Move datefiltering to own function

--- backend/controllers/retentionrateController.js
+++ backend/controllers/retentionrateController.js
@@ -5,6 +5,16 @@ const user_profiles = db.user_profiles
 const user_activities = db.user_activities
 const controller = require('./controller')
 
+const addDateFilterToQuery = (query, startDate, endDate) => {
+  if (startDate && endDate) {
+    query.where['created_at'] = { [Op.between]: [startDate, endDate] }
+  } else if (startDate) {
+    query.where['created_at'] = { [Op.gte]: startDate }
+  } else if (endDate) {
+    query.where['created_at'] = { [Op.lte]: endDate }
+  }
+}
+
 /**
  * Returns retention rates as in how long does user use app actively
  *
@@ -17,7 +27,6 @@ const controller = require('./controller')
 const findRetentionRates = async (organisation, withCaregiver, startDate, endDate) => {
   const userIds = await controller.findAllUsers(organisation, withCaregiver)
   const userIdsArray = userIds.map(user => user.user_id)
-  let userActivities
 
   let activitiesQuery = {
     where: {
@@ -29,15 +38,8 @@ const findRetentionRates = async (organisation, withCaregiver, startDate, endDat
     attributes: ['id', 'user_id', 'created_at']
   }
 
-  if (startDate !== '' && endDate !== '') {
-    activitiesQuery.where['created_at'] = { [Op.between]: [startDate, endDate] }
-  } else if (startDate !== '') {
-    activitiesQuery.where['created_at'] = { [Op.gte]: startDate }
-  } else if (endDate !== '') {
-    activitiesQuery.where['created_at'] = { [Op.lte]: endDate }
-  }
-
-  userActivities = await user_activities.findAll(activitiesQuery)
+  addDateFilterToQuery(activitiesQuery, startDate, endDate)
+  const userActivities = await user_activities.findAll(activitiesQuery)
 
   const userProfiles = await user_profiles.findAll({
     where: {
