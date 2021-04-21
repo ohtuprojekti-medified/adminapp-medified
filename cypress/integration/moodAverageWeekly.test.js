05020e0f0c5b8f583624a24230f3e3278ea541d0
hanranti <hanranti@cs.helsinki.fi>
2021-04-21 15:40:18 +0300

    Edit tests and jsdoc

--- cypress/integration/moodAverageWeekly.test.js
+++ cypress/integration/moodAverageWeekly.test.js
@@ -1,17 +1,17 @@
 /**
- * Cypress tests for MoodAverage chart
+ * Cypress tests for AverageMoodWeekly chart
  *
- * @module cypress/integration/moodAverage_spec
+ * @module cypress/integration/AverageMoodWeekly_spec
  * @requires cypress
  */
 
 /**
  * Describe tests for mood average page
  *
- * @name MoodAverage
+ * @name AverageMoodWeekly
  * @type {object}
- * @memberof module:cypress/integration/moodAverage_spec
- * @param {string} describe - MoodAverage chart
+ * @memberof module:cypress/integration/AverageMoodWeekly_spec
+ * @param {string} describe - AverageMoodWeekly chart
  * @param {object} tests - Test code
  */
 describe('MoodAverage chart', function () {
@@ -21,7 +21,7 @@ describe('MoodAverage chart', function () {
    *
    * @name beforeEach
    * @type {object}
-   * @memberof module:cypress/integration/moodAverage_spec
+   * @memberof module:cypress/integration/AverageMoodWeekly_spec
    * @inner
    * @param {object} functionBeforeEach - Function to be run before each test
    */
@@ -34,7 +34,7 @@ describe('MoodAverage chart', function () {
    *
    * @name afterEach
    * @type {object}
-   * @memberof module:cypress/integration/moodAverage_spec
+   * @memberof module:cypress/integration/AverageMoodWeekly_spec
    * @inner
    * @param {object} functionAfterEach - Function to be run before each test
    */
@@ -43,16 +43,120 @@ describe('MoodAverage chart', function () {
   })
 
   /**
-   * Test that mood average page exists
+   * Test that AverageMoodWeekly page exists
    *
-   * @name MoodAverage_exists
+   * @name AverageMoodWeekly_exists
    * @type {object}
-   * @memberof module:cypress/integration/moodAverage_spec
+   * @memberof module:cypress/integration/AverageMoodWeekly_spec
    * @inner
    * @param {string} describe - exists
    * @param {object} testFunction - Function that runs test
    */
   it('exists', function () {
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Average mood weekly')
+  })
+
+  /**
+   * Test that Average mood weekly page exists after checking only patients with caregiver
+   *
+   * @name AverageMoodWeekly_exists_after_checking_only_patients_with_caregiver
+   * @type {object}
+   * @memberof module:cypress/integration/AverageMoodWeekly_spec
+   * @inner
+   * @param {string} describe - exists after checking only patients with caregiver
+   * @param {object} testFunction - Function that runs test
+   */
+  it('exists after checking only patients with caregivers', function () {
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Average mood weekly')
+    cy.contains('Filter').click()
+    cy.get('[data-testid="filter-checkbox"]').check()
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Average mood weekly')
+  })
+
+  /**
+   * Test that Average mood weekly page exists after selecting start date
+   *
+   * @name AverageMoodWeekly_exists_after_selecting_start_date
+   * @type {object}
+   * @memberof module:cypress/integration/AverageMoodWeekly_spec
+   * @inner
+   * @param {string} describe - exists after selecting start date
+   * @param {object} testFunction - Function that runs test
+   */
+  it('exists after selecting start date in filters', function () {
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Average mood weekly')
+    cy.contains('Filter').click()
+    cy.get('[data-testid="startDate-checkbox"]').check()
+    cy.get('[data-testid="startDate-date"]').type('2020-06-01')
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Average mood weekly')
+  })
+
+  /**
+   * Test that Average mood weekly page exists after selecting end date
+   *
+   * @name AverageMoodWeekly_exists_after_selecting_end_date
+   * @type {object}
+   * @memberof module:cypress/integration/AverageMoodWeekly_spec
+   * @inner
+   * @param {string} describe - exists after selecting end date
+   * @param {object} testFunction - Function that runs test
+   */
+  it('exists after selecting end date in filters', function () {
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Average mood weekly')
+    cy.contains('Filter').click()
+    cy.get('[data-testid="endDate-checkbox"]').check()
+    cy.get('[data-testid="endDate-date"]').type('2020-11-01')
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Average mood weekly')
+  })
+
+  /**
+   * Test that Average mood weekly page exists after selecting start date and end date
+   *
+   * @name AverageMoodWeekly_exists_after_selecting_start_date_and_end_date
+   * @type {object}
+   * @memberof module:cypress/integration/AverageMoodWeekly_spec
+   * @inner
+   * @param {string} describe - exists after selecting start date and end date
+   * @param {object} testFunction - Function that runs test
+   */
+  it('exists after selecting start date and end date in filters', function () {
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Average mood weekly')
+    cy.contains('Filter').click()
+    cy.get('[data-testid="startDate-checkbox"]').check()
+    cy.get('[data-testid="startDate-date"]').type('2020-06-01')
+    cy.get('[data-testid="endDate-checkbox"]').check()
+    cy.get('[data-testid="endDate-date"]').type('2020-11-01')
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Average mood weekly')
+  })
+
+  /**
+   * Test that Average mood weekly page exists after checking only patients with caregivers, start date and end date
+   *
+   * @name AverageMoodWeekly_exists_after_checking_only_patients_with_caregivers_start_date_and_end_date
+   * @type {object}
+   * @memberof module:cypress/integration/AverageMoodWeekly_spec
+   * @inner
+   * @param {string} describe - exists after selecting only patients with caregiver start date and end date
+   * @param {object} testFunction - Function that runs test
+   */
+  it('exists after checking only patients with caregivers, start date and end date in filters', function () {
+    cy.contains('Adminapp for monitoring moods')
+    cy.contains('Average mood weekly')
+    cy.contains('Filter').click()
+    cy.get('[data-testid="filter-checkbox"]').check()
+    cy.get('[data-testid="startDate-checkbox"]').check()
+    cy.get('[data-testid="startDate-date"]').type('2020-06-01')
+    cy.get('[data-testid="endDate-checkbox"]').check()
+    cy.get('[data-testid="endDate-date"]').type('2020-11-01')
     cy.contains('Adminapp for monitoring moods')
     cy.contains('Average mood weekly')
   })
